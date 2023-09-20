275ff92854a3a4cea4e647e004a12313c9e2d167
Valtteri Raatikainen <valtteri.raatikainen1@gmail.com>
2023-09-20 14:30:31 +0300

    Tehtävä tehty

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button, Alert, TextInput, Image } from 'react-native';
 
 export default function App() {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+
+  const plusPressed = () => {
+    Alert.alert('Result: ' + (Number(num1) + Number(num2)));
+  };
+  const minusPressed = () => {
+    Alert.alert('Result: ' + (Number(num1) - Number(num2)));
+  };
   return (
     <View style={styles.container}>
-      <Text>Testi</Text>
-      <StatusBar style="auto" />
+      <TextInput style={styles.input} onChangeText={num1 => setNum1(num1)} value={num1} inputMode='numeric' />
+      <TextInput style={styles.input} onChangeText={num2 => setNum2(num2)} value={num2} inputMode='numeric' />
+      <View style={styles.buttonContainer}>
+        <Button onPress={plusPressed} title="+" />
+        <Button onPress={minusPressed} title="-" />
+      </View>
     </View>
+
   );
 }
 
@@ -17,4 +31,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 250,
+    height: 100
+  },
+  input: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  }
 });
