292c3b429c480a94f26c1ae99f7b185c1b840638
Valtteri Raatikainen <valtteri.raatikainen1@gmail.com>
2024-01-21 12:46:43 +0200

    Tehtävää korjattu

--- App.js
+++ App.js
@@ -1,18 +1,22 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, Button, Alert, TextInput, Image } from 'react-native';
+import { StyleSheet, View,Button, Text, TextInput} from 'react-native';
 
 export default function App() {
   const [num1, setNum1] = useState('');
   const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
 
   const plusPressed = () => {
-    Alert.alert('Result: ' + (Number(num1) + Number(num2)));
+    setResult(Number(num1) + Number(num2));
   };
+
   const minusPressed = () => {
-    Alert.alert('Result: ' + (Number(num1) - Number(num2)));
+    setResult(Number(num1) - Number(num2));
   };
+
   return (
     <View style={styles.container}>
+      <Text>Result: {result}</Text>
       <TextInput style={styles.input} onChangeText={num1 => setNum1(num1)} value={num1} inputMode='numeric' />
       <TextInput style={styles.input} onChangeText={num2 => setNum2(num2)} value={num2} inputMode='numeric' />
       <View style={styles.buttonContainer}>
@@ -20,7 +24,6 @@ export default function App() {
         <Button onPress={minusPressed} title="-" />
       </View>
     </View>
-
   );
 }
 
@@ -31,10 +34,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  image: {
-    width: 250,
-    height: 100
-  },
   input: {
     width: 200,
     borderColor: 'gray',
@@ -43,5 +42,13 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flexDirection: 'row',
-  }
+    marginTop: 10,
+  },
+  historyItem: {
+    backgroundColor: '#eee',
+    padding: 10,
+    marginVertical: 5,
+    width: 200,
+    alignItems: 'center',
+  },
 });
